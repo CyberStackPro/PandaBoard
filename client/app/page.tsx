0f72c015412983c2ac47f10ddf9917bef48f14e6
CyberStackPro <yeabsragebriel@gmail.com>
2024-12-18 12:03:46 +0300

    [CLIENT] add shadcn ui

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
 };
 
 export default page;
