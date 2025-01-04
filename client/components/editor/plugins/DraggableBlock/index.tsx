3b5223e69eab26018dea31164d50ac93a858c37e
CyberStackPro <yeabsragebriel@gmail.com>
2025-01-04 22:48:05 +0300

    ♿️ [CLIENT]
    improve the draggable ui in the content editable

--- client/components/editor/plugins/DraggableBlock/index.tsx
+++ client/components/editor/plugins/DraggableBlock/index.tsx
@@ -1,7 +1,8 @@
 import { DraggableBlockPlugin_EXPERIMENTAL } from "@lexical/react/LexicalDraggableBlockPlugin";
 import { useRef } from "react";
-import { GripVertical } from "lucide-react";
+import { GripVertical, Plus } from "lucide-react";
 import "./index.css";
+import { Button } from "@/components/ui/button";
 
 const DRAGGABLE_BLOCK_MENU_CLASSNAME = "draggable-block-menu";
 
@@ -25,15 +26,23 @@ export default function DraggableBlockPlugin({
       menuComponent={
         <div
           ref={menuRef}
-          className={`${DRAGGABLE_BLOCK_MENU_CLASSNAME} opacity-0 group-hover:opacity-100 absolute -left-6 top-1/2 -translate-y-1/2 flex items-center justify-center p-1 cursor-grab active:cursor-grabbing hover:bg-accent/50 rounded-md z-[100]`}
+          className={`${DRAGGABLE_BLOCK_MENU_CLASSNAME}  opacity-0 group-hover:opacity-100 absolute left-0 top-0 transition-transform flex items-center justify-center p-1 cursor-grab active:cursor-grabbing hover:bg-accent rounded-md z-[100]`}
         >
-          <GripVertical className="h-4 w-4 text-muted-foreground group-hover:text-foreground" />
+          {/* <Button
+            variant="ghost"
+            className="h-8 w-8 p-0 text-muted-foreground hover:text-foreground"
+            onClick={() => console.log("Add new block")}
+          >
+            <Plus className="h-4 w-4" />
+          </Button> */}
+
+          <GripVertical className="size-4  text-muted-foreground group-hover:text-foreground" />
         </div>
       }
       targetLineComponent={
         <div
           ref={targetLineRef}
-          className="draggable-block-target-line bg-primary"
+          className="draggable-block-target-line "
           style={{ height: "3px" }}
         />
       }
