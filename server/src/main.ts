cb2dd43dcf9e0170648228f532966b771b8b7446
CyberStackPro <yeabsragebriel@gmail.com>
2025-01-16 10:48:46 +0300

    🎨 [SERVER]
    add http exception

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors({
     origin: 'http://localhost:3000',
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
